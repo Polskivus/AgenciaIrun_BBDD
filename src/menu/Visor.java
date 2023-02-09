97957033b22fbb055111e89787633e7aa27b4ad0
Polskivus <polako_irun@icloud.com>
2023-02-09 13:17:39 +0100

    AÑADIDO MOSTRAR ARRAY CLIENTES PARA QUE SE VEAN TODOS LOS CLIENTES REGISTRADOS

--- src/menu/Visor.java
+++ src/menu/Visor.java
@@ -1,5 +1,19 @@
 package menu;
 
+import java.util.ArrayList;
+
+import objetos.Cliente;
+
 public class Visor {
 
+	public static void mostrarArrayClientes(ArrayList<Cliente> clientes) {
+		
+		for (Cliente cliente : clientes) {
+			
+			System.out.println(cliente);
+			
+		}
+		
+	}
+	
 }
