3a7f167333d5010b93e55c6a7dc7e0e87f330c31
Polskivus <polako_irun@icloud.com>
2023-02-13 13:29:35 +0100

    Se ha creado el insertar hotel

--- src/gestores/GestorHotel.java
+++ src/gestores/GestorHotel.java
@@ -2,7 +2,9 @@ package gestores;
 
 import java.util.Scanner;
 
+import menu.FormularioDatos;
 import menu.Menu;
+import objetos.Hotel;
 
 public class GestorHotel {
 
@@ -13,49 +15,58 @@ public class GestorHotel {
 	public static void run(Scanner sc) {
 
 		GestorDDBB gestorDDBB = new GestorDDBB();
-		
+
 		int opcHotel;
-		
+
 		do {
-			
+
 			Menu.menuHoteles();
-			
+
 			opcHotel = Integer.parseInt(sc.nextLine());
-			
-			while(opcHotel < Menu.SALIR || opcHotel > Menu.VISUALIZAR_HOTEL) {
-				
+
+			while (opcHotel < Menu.SALIR || opcHotel > Menu.VISUALIZAR_HOTEL) {
+
 				System.out.println("!Error¡ Introduce un valor valido");
 
 				Menu.menuClientes();
 
 				opcHotel = Integer.parseInt(sc.nextLine());
-				
-			};
-			
+
+			}
+			;
+
 			switch (opcHotel) {
 			case Menu.INSERTAR_HOTEL:
+
+				Hotel hotel = new Hotel();
+
+				gestorDDBB.abrirConexion();
+				hotel = FormularioDatos.insertarHotel(sc);
+				gestorDDBB.insertarHotel(hotel);
+				gestorDDBB.cerrarConexion();
+				
+				System.out.println("----------HOTEL AÑADIDO----------");
 				
 				break;
 
 			case Menu.ELIMINAR_HOTEL:
-				
+
 				break;
-				
+
 			case Menu.MODIFICAR_HOTEL:
-				
+
 				break;
-			
+
 			case Menu.VISUALIZAR_HOTEL:
-				
+
 				break;
-			
+
 			case Menu.SALIR:
 				break;
 			}
-			
-			
-		}while(opcHotel != Menu.SALIR);
-		
+
+		} while (opcHotel != Menu.SALIR);
+
 	}
 
 }
